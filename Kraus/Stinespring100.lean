bfb44b7bd5de9e67e924d02ba519c50656f041a2
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2026-03-12 18:17:00 -1000

    Stinespring100 with new mathlib

--- Kraus/Stinespring100.lean
+++ Kraus/Stinespring100.lean
@@ -29,8 +29,10 @@ lemma stinespringForm_eq {m r : ℕ}
     (ρ : Matrix (Fin m) (Fin m) ℂ) :
     partialTraceRight (stinespringDilation K ρ) = ∑ i, K i * ρ * (K i)ᴴ := by
   ext u v
-  repeat rw [Finset.sum_apply]
   simp only [partialTraceRight, stinespringDilation, V, kronecker, Fin.isValue]
+  have :  (∑ i, K i * ρ * (K i)ᴴ) u v =  ∑ i, (K i * ρ * (K i)ᴴ) u v := by
+    exact sum_apply u v Finset.univ fun c ↦ K c * ρ * (K c)ᴴ
+  rw [this]
   congr
   ext w
   simp only [kroneckerMap, single, Fin.isValue, of_apply,
@@ -40,5 +42,30 @@ lemma stinespringForm_eq {m r : ℕ}
   ext j
   repeat rw [Matrix.mul_apply]
   simp only [Fin.isValue, conjTranspose_apply, star_def]
-  repeat rw [Finset.sum_apply]
+  rw [Matrix.sum_apply]
+  have (x : Fin r) :
+      (fun i j ↦ if x = i.2 ∧ 0 = j.2 then K x i.1 j.1 else 0 : Fin m × Fin r → Fin m × Fin 1 → ℂ)
+    = (fun i j ↦ if x = i.2 then K i.2 i.1 j.1 else 0 : Fin m × Fin r → Fin m × Fin 1 → ℂ) := by
+        ext i j
+        split_ifs with g₀ g₁ g₂
+        · rw [g₁]
+        · tauto
+        · omega
+        rfl
+  simp_rw [this]
+  simp only [Fin.isValue, of_apply, Finset.sum_ite_eq', Finset.mem_univ, ↓reduceIte,
+    mul_eq_mul_right_iff, map_eq_zero]
+  left
+  congr
+  ext a
+  congr
+  rw [of]
+  suffices (of (∑ x, fun i (j : Fin m × Fin 1) ↦ if x = i.2 then K i.2 i.1 j.1 else 0))
+    (u, w) (a, 0) =
+      K w u a by
+    rw [← this]
+    let G (x : Fin r) := (fun (i : Fin m × Fin r) (j : Fin m × Fin 1) ↦
+        if x = i.2 then K i.2 i.1 j.1 else 0 : Fin m × Fin r → Fin m × Fin 1 → ℂ)
+    change (∑ x, of G x) (u, w) (a, 0) = Matrix.of (∑ x : Fin r, G x) (u, w) (a, 0)
+    simp
   simp
